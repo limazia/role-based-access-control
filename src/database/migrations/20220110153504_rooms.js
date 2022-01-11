35503b2033db63581d530ddd34b034b6f02d538b
Acacio de Lima <limadeacacio@gmail.com>
2022-01-11 13:44:53 -0300

    update

--- src/database/migrations/20220110153504_rooms.js
+++ src/database/migrations/20220110153504_rooms.js
@@ -1,9 +1,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable("rooms", (table) => {
     table.string("room_id").primary().unique().notNullable();
-    table.string("room_name").notNullable();
-    table.string("room_description").notNullable();
-    table.string("room_room", 0).notNullable();
+    table.string("room_title", 25).notNullable();
+    table.string("room_name", 30).notNullable();
+    table.integer("room_private", 0).notNullable();
     table.string("room_photo");
 
     table.timestamp("updateAt").defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
