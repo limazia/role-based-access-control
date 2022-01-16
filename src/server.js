f7941f4a6d899bd041b71943b6dc05364a9c232a
Acacio de Lima <limadeacacio@gmail.com>
2022-01-16 03:52:31 -0300

    update

--- src/server.js
+++ src/server.js
@@ -7,27 +7,22 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 const flash = require("connect-flash");
-const subdomain = require("express-subdomain");
+
+const app = express();
+
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
 const routes = require("./routes");
 const { env } = require("./helpers/utils.helper");
 const { handleError } = require("./helpers/error.helper");
 const { AppConfig, AuthConfig } = require("./config");
 
-const app = express();
-
 app.set("view engine", "ejs");
 app.set("views", [
   __dirname + "/resources/views",
   __dirname + "/resources/views/errors",
 ]);
-
-app.use(subdomain('cdn', express.static(path.join(__dirname, "../cdn"))));
-app.use(express.static(path.join(__dirname, "../cdn"), {
-  index: false,
-  redirect: false
-}));
-//app.use("/cdn", express.static(path.resolve(__dirname, '../', "cdn")));
 
 if (env("APP_DEBUG", false) === true) app.use(morgan(':method :url :status :response-time ms'));
 app.use(cookieParser());
@@ -52,13 +47,40 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(flash());
 app.use(express.static(path.join(__dirname, "/public")));
+app.use("/cdn", express.static(path.resolve(__dirname, '../', "cdn")));
 app.use(routes); 
 app.use((err, req, res, next) => {
   handleError(err, res);
 });
 
+// Cria um array com as mensagens
+let users = [];
+let messages = [];
+
+// Quando um client se conectar
+io.on("connection", socket => {
+    console.log(`Socket conectado: ${socket.id}`);
+
+    // Envia uma mensagem ao socket
+    socket.emit("previousMessages", messages);
+
+    // Ao executar a função no Front-end, recebemos no Back-end pra tratar da maneira que quisermos
+    socket.on("sendMessage", data => {
+      // Envia a informação pro array messages
+      messages.push(data);
+      
+      // Envia para todos os sockets
+      socket.broadcast.emit("receivedMessage", data);
+    });
+  
+    // client disconnected
+    socket.on('disconnect', function() {
+      console.log(`${socket.id} is disconnected.`);
+    });
+});
+
 app.locals = AppConfig.locals;
 
-app.listen(AppConfig.port, () => {
+server.listen(AppConfig.port, () => {
   console.log(`Server running in ${env("APP_URL", "http://localhost")}:${env("APP_PORT", 3333)}`);
 });
